d728a41283f437bfc4a588ecc060abb6ff9ba0ae
Anushree Kamath <anushreekamath1@gmail.com>
2026-04-29 11:25:07 +0530

    updated ci/cd for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import cookieParser from 'cookie-parser';
 
+//trying CI/CD for backend
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
